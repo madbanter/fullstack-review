4ff9cbcfe81614cfe644f53109cc99723747b4ec
Adam Brent <adam.brent@gmail.com>
2021-01-09 14:59:58 -0800

    More setup.

--- database/index.js
+++ database/index.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  name: String,
+  owner: String,
+  html_url: String,
+  created_at: Date,
+  updated_at: Date,
+  watchers: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
@@ -11,6 +16,8 @@ let save = (/* TODO */) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  // create new document from model containing input
+  // save document to db
 }
 
 module.exports.save = save;
---
+++ repoKeys.js
@@ -0,0 +1,5 @@
+const repoKeys = {
+  name, owner.login, html_url, created_at, updated_at, watchers
+};
+
+export default repoKeys;
